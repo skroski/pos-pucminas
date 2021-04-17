0ebfb1b8fb0126e66cc5c874d50d2e94c0856206
daniel.skroski <daniel@duplod.com.br>
2021-04-17 13:51:19 -0300

    Atualizações dos Exercícios de JavaScript

--- javascript/unidade1/capitulo1/prog-atividade.js
+++ javascript/unidade1/capitulo1/prog-atividade.js
@@ -1,7 +1,7 @@
 let n = 15; // os comandos abaixo poder funcionar para quaisquer valores de n
 
 let primo = true;
-for(let i = 2; i > 1;  i--){
+for(let i = 2; i > 1;  i--){ // Ainda aguardando a resolução do problema.
   if(n%i == 0) // Testa se n é divisível por i
     primo = false;
 }
