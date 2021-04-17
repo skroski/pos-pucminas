43657bd67dfa3277adcdc963f835388f56cb9a56
daniel.skroski <daniel@duplod.com.br>
2021-04-17 13:50:00 -0300

    Exercícios da Pós em JS

---
+++ javascript/unidade1/capitulo1/prog-atividade.js
@@ -0,0 +1,8 @@
+let n = 15; // os comandos abaixo poder funcionar para quaisquer valores de n
+
+let primo = true;
+for(let i = 2; i > 1;  i--){
+  if(n%i == 0) // Testa se n é divisível por i
+    primo = false;
+}
+console.log(`${n} ${!primo?"não ":""} é um número primo.`);
